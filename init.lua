e2c9b05ddcf8e31a0dfc3ffecf771eb3562ba07e
Fikry Fahrezy <ffahrezi69@gmail.com>
2023-04-20 14:15:58 +0700

    add lsp config

--- init.lua
+++ init.lua
@@ -27,6 +27,24 @@ return {
   },
 
   lsp = {
+    config = {
+      denols = function(opts)
+        opts.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
+        return opts
+      end,
+      tsserver = function(opts)
+        opts.root_dir = require("lspconfig.util").root_pattern("package.json")
+        return opts
+      end,
+      eslint = function(opts)
+        opts.root_dir = require("lspconfig.util").root_pattern("package.json", ".eslintrc.json", ".eslintrc.js")
+        return opts
+      end,
+      tailwindcss = function(opts)
+        opts.root_dir = require("lspconfig.util").root_pattern('tailwind.config.js', 'tailwind.config.ts')
+	return opts
+      end,
+    },
     -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
